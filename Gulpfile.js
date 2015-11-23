0c2a1719a2d0a02d3817912f04cf4515752f1770
Lauren <laurenortencio@gmail.com>
2015-11-22 16:45:17 -0800

    first commit

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+	gulp.src('public/sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./public/css/'));
+
+});
+
+//Watch tash 
+gulp.task('default', function() {
+	gulp.watch('./public/sass/**/*.scss',['styles']);
+})
